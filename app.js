dcb47d0366edb486f29ef69c5ffe5053350e31f1
Sam Barker <samuel.e.barker@googlemail.com>
2020-03-20 16:31:24 +0000

    finished a basic CLI

--- app.js
+++ app.js
@@ -1,20 +1,93 @@
 const API = require("./lib/API");
+const readlineSync = require("readline-sync");
 
-const books = API.read("books");
-console.log(books);
+function averageRating(book) {
+  let total = 0;
+  for (const review of book.reviews) {
+    total += parseInt(review.rating);
+  }
+  return total / book.reviews.length;
+}
 
-const book = API.read("books", 1);
-console.log(book);
+function displayBooks(books) {
+  for (const book of books) {
+    if (book.reviews.length > 0) {
+      console.log(
+        `--- ${book.id}: ${book.title}, rating: ${averageRating(book)}`
+      );
+    } else {
+      console.log(`--- ${book.id}: ${book.title}, no reviews yet!`);
+    }
+  }
+}
 
-const newBook = {
-  title: "Book about Ruby"
-};
+function displayBook(book) {
+  console.log(`-- ${book.title} --`);
+  for (const review of book.reviews) {
+    console.log(`${review.content} - Rating: ${review.rating}`);
+  }
+}
 
-API.create("books", newBook);
+function chooseABook(books) {
+  for (const book of books) {
+    console.log(`--- ${book.id}: ${book.title}`);
+  }
+  const bookChoice = readlineSync.question(
+    "Which book would you like to review? "
+  );
+  const book = API.read("books", bookChoice);
+  if (book !== undefined) {
+    return book;
+  } else {
+    console.log("Ooops we can't find that book!");
+    return chooseABook(books);
+  }
+}
 
-API.update("books", {
-  id: 1,
-  title: "Good Book about Javascript"
-});
+function mainMenu() {
+  console.log("----------------");
+  console.log("---- AMAZON ----");
+  console.log("----------------");
+  console.log("1. View our books");
+  console.log("2. Leave a review");
+  console.log("----------------");
 
-API.destroy("books", 3);
+  const choice = readlineSync.question("Please choose an option ");
+
+  if (choice === "1") {
+    console.log("----------------");
+    console.log("- ALL OUR BOOKS -");
+    console.log("----------------");
+
+    const books = API.read("books");
+    displayBooks(books);
+    mainMenu();
+  } else if (choice === "2") {
+    console.log("-----------------");
+    console.log("- CHOOSE A BOOK -");
+    console.log("-----------------");
+
+    const books = API.read("books");
+    const book = chooseABook(books);
+    displayBook(book);
+    const rating = readlineSync.question("What is your rating? ");
+    const content = readlineSync.question("Please write your review ");
+    book.reviews.push({
+      rating: rating,
+      content: content
+    });
+
+    API.update("books", book);
+
+    console.log("----------------");
+    console.log("Thanks for leaving a review!");
+    console.log("----------------");
+
+    mainMenu();
+  } else {
+    console.log("Sorry we didn't recognise that choice!");
+    mainMenu();
+  }
+}
+
+mainMenu();
